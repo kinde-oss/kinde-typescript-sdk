5ea010852e73dbca80ce37699490974763776c01
Peter Phanouvong <peterphanouvong@gmail.com>
2023-10-30 11:14:38 +1100

    feat: include auth url params in payload

--- lib/sdk/oauth2-flows/types.ts
+++ lib/sdk/oauth2-flows/types.ts
@@ -1,63 +1,64 @@
 export enum GrantType {
-  AUTHORIZATION_CODE = 'AUTHORIZATION_CODE',
-  CLIENT_CREDENTIALS = 'CLIENT_CREDENTIALS',
-  PKCE = 'PKCE',
+	AUTHORIZATION_CODE = 'AUTHORIZATION_CODE',
+	CLIENT_CREDENTIALS = 'CLIENT_CREDENTIALS',
+	PKCE = 'PKCE',
 }
 
 export interface OAuth2CodeExchangeErrorResponse {
-  error?: string;
-  error_description?: string;
+	error?: string;
+	error_description?: string;
 }
 
 export interface OAuth2CodeExchangeResponse {
-  access_token: string;
-  expires_in: string;
-  token_type: string;
-  refresh_token: string;
-  id_token: string;
-  scope: string;
+	access_token: string;
+	expires_in: string;
+	token_type: string;
+	refresh_token: string;
+	id_token: string;
+	scope: string;
 }
 
 export interface OAuth2CCTokenErrorResponse {
-  error?: string;
-  error_description?: string;
+	error?: string;
+	error_description?: string;
 }
 
 export interface OAuth2CCTokenResponse {
-  access_token: string;
-  expires_in: string;
+	access_token: string;
+	expires_in: string;
 }
 
 export interface OAuth2FlowOptions {
-  clientId: string;
-  logoutRedirectURL: string;
-  authDomain: string;
-  audience?: string;
-  scope?: string;
+	clientId: string;
+	logoutRedirectURL: string;
+	authDomain: string;
+	audience?: string;
+	scope?: string;
 }
 
 export interface SDKHeaderOverrideOptions {
-  framework?: string;
-  frameworkVersion?: string;
+	framework?: string;
+	frameworkVersion?: string;
 }
 
 export interface AuthorizationCodeOptions
-  extends OAuth2FlowOptions,
-    SDKHeaderOverrideOptions {
-  redirectURL: string;
+	extends OAuth2FlowOptions,
+		SDKHeaderOverrideOptions {
+	redirectURL: string;
 }
 
 export interface ClientCredentialsOptions
-  extends OAuth2FlowOptions,
-    SDKHeaderOverrideOptions {
-  clientSecret: string;
+	extends OAuth2FlowOptions,
+		SDKHeaderOverrideOptions {
+	clientSecret: string;
 }
 
 export interface AuthURLOptions {
-  start_page?: 'registration' | 'login';
-  is_create_org?: boolean;
-  org_name?: string;
-  org_code?: string;
-  state?: string;
-  post_login_redirect_url?: string;
+	start_page?: 'registration' | 'login';
+	is_create_org?: boolean;
+	org_name?: string;
+	org_code?: string;
+	state?: string;
+	post_login_redirect_url?: string;
+	authUrlParams?: Record<string, string>;
 }
