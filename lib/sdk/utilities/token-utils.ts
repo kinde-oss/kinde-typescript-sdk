391cb3d0449ea21ba85d6b08252ee1a62424061b
Dave Berner <davidajberner@gmail.com>
2023-11-15 17:07:24 +1100

    feat: replace atob with library due to unicode issues

--- lib/sdk/utilities/token-utils.ts
+++ lib/sdk/utilities/token-utils.ts
@@ -1,19 +1,7 @@
+import {jwtDecode} from 'jwt-decode';
 import type { TokenCollection, UserType, TokenType } from './types.js';
 import { type SessionManager } from '../session-managers/index.js';
 
-/**
- * Parses a provided JWT token to extract the payload segment of said
- * token.
- * @param token {string}
- * @returns {any}
- */
-const getTokenPayload = (token: string): any => {
-  try {
-    return JSON.parse(atob(token.split('.')[1]));
-  } catch (e) {
-    return null;
-  }
-};
 
 /**
  * Extracts the payload from the provided idToken and saves the extracted
@@ -26,7 +14,7 @@ const commitUserToMemoryFromToken = async (
   sessionManager: SessionManager,
   idToken: string
 ): Promise<void> => {
-  const idTokenPayload = getTokenPayload(idToken);
+  const idTokenPayload = jwtDecode(idToken);
   const user: UserType = {
     family_name: idTokenPayload.family_name,
     given_name: idTokenPayload.given_name,
@@ -49,7 +37,7 @@ export const commitTokenToMemory = async (
   token: string,
   type: TokenType
 ): Promise<void> => {
-  const tokenPayload = getTokenPayload(token);
+  const tokenPayload = jwtDecode(token);
   await sessionManager.setSessionItem(type, token);
   if (type === 'access_token') {
     await sessionManager.setSessionItem('access_token_payload', tokenPayload);
@@ -138,6 +126,6 @@ export const commitUserToMemory = async (
 export const isTokenExpired = (token: string | null): boolean => {
   if (!token) return true;
   const currentUnixTime = Math.floor(Date.now() / 1000);
-  const tokenPayload = getTokenPayload(token);
+  const tokenPayload = jwtDecode(token);
   return currentUnixTime >= tokenPayload.exp;
 };
