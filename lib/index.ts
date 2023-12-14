416684c3fe8e5f064b09eb764a354d1d29d2f76c
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2023-12-15 00:01:22 +0500

    chore: exposing all management apis as a single object

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,14 @@
 /* tslint:disable */
 /* eslint-disable */
+import * as apis from './apis/index.js';
+import * as models from './models/index.js';
+
 export * from './runtime.js';
 export * from './apis/index.js';
 export * from './models/index.js';
 export * from './sdk/index.js';
+
+export const managementApi = {
+  apis, models
+};
+
