ebc51a08617403ada3f2ab5a391a959d8ff79ef2
Daniel Rivers <me@danielrivers.com>
2024-03-06 21:54:01 +0000

    fix: odd random string lengths

--- lib/__tests__/sdk/utilities/generateRandomString.spec.ts
+++ lib/__tests__/sdk/utilities/generateRandomString.spec.ts
@@ -6,6 +6,12 @@ describe('validateClientSecret', () => {
     expect(result.length).toBe(25);
   });
 
+
+  it('should return false for invalid secrets - odd length', () => {
+    const result = generateRandomString(47);
+    expect(result.length).toBe(47);
+  });
+
   it('should return false for invalid secrets', () => {
     const result = generateRandomString(50);
     expect(result.length).toBe(50);
--- lib/sdk/utilities/random-string.ts
+++ lib/sdk/utilities/random-string.ts
@@ -6,7 +6,13 @@ import { getRandomValues } from 'uncrypto';
  * @returns {string} required secret
  */
 export const generateRandomString = (length: number = 28): string => {
-  const array = new Uint32Array(length);
+  const bytesNeeded = Math.ceil(length / 2);
+  const array = new Uint32Array(bytesNeeded);
   getRandomValues(array);
-  return Array.from(array, (dec) => ('0' + dec.toString(16)).slice(-2)).join('').slice(0, length);
+  let result = Array.from(array, (dec) => ('0' + dec.toString(16)).slice(-2)).join('');
+  if (length % 2 !== 0) {
+    // If the requested length is odd, remove the last character to adjust the length
+    result = result.slice(0, -1);
+  }
+  return result;
 };
