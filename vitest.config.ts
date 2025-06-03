faf3f8bd9b2577c2060317daa6e5846197960654
Daniel Rivers <me@danielrivers.com>
2025-06-03 23:55:23 +0100

    chore: vitest coverage exclude

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
     include: ['lib/**/*.{test,spec}.ts'],
     exclude: ['lib/__tests__/mocks.ts'],
     globals: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: ['lib/__tests__/mocks.ts', 'dist', 'dist-cjs', 'lib/models', 'lib/apis']
+    },
   },
   resolve: {
     alias: {
